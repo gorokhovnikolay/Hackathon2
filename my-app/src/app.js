e6bf4285babaf863520c965b87a374e50f910f73
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2024-03-29 17:56:03 +0300

    i styled-components

--- my-app/src/app.js
+++ my-app/src/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { Main } from './pages';
 
 export const App = () => {
 	return (
 		<Routes>
-			<Route path="/" element={<div>Главная</div>} />
+			<Route path="/" element={<Main />} />
 			<Route path="/favorits" element={<div>Избранные</div>} />
 			<Route path="/student" element={<div>Студент</div>} />
 			<Route path="*" element={<div>Ошибка</div>} />
---
+++ my-app/src/pages/main/main.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const MainContainer = () => {
+	return <div>Main</div>;
+};
+
+export const Main = styled(MainContainer)``;
