1f3b8bd9b4262ba9413cf91124717181492b0b03
Nikolay Gorokhov <nikolay.gorokhov@ya.ru>
2024-03-30 14:19:56 +0300

    реализовать компонент badge

--- my-app/src/components/header/header.js
+++ my-app/src/components/header/header.js
@@ -14,6 +14,7 @@ const HeaderContainer = ({ className }) => {
 export const Header = styled(HeaderContainer)`
 	height: 50px;
 	background: #474747;
+	margin-bottom: 15px;
 	& .header-menu {
 		width: 1000px;
 		margin: 0 auto;
